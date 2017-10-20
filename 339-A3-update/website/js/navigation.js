ad29df810ee9ea41a28887297fbf10b30e3faa9d
Jayant Tailor <jayantt@sfu.ca>
2017-10-20 10:55:16 -0700

    Disable mobile navigation for larger sizes
    Check the screen width to display the mobile navigation options on click. If the navigation is not on the bottom (mobile) then the onlick function shouldn't do anything

--- 339-A3-update/website/js/navigation.js
+++ 339-A3-update/website/js/navigation.js
@@ -5,5 +5,9 @@ function navigationClose(navType) {
 
 function navigationOpen(navType) {
   var id = "nav-list-mobile-" + navType;
-  document.getElementById(id).style.display = 'flex';
+  // This is when the media query changes so the navigation is on the bottom
+  if (screen.width < 517) {
+    document.getElementById(id).style.display = 'flex';
+  }
+
 }
